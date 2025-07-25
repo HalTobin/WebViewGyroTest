638e47223eb398eb1c54116b5512ccc206809356
Alexis Aneas <alexis.aneas@outlook.fr>
2025-07-25 17:09:39 +0200

    Initial Commit

---
+++ GyroInWebViewTest/GyroInWebViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GyroInWebViewTestApp.swift
+//  GyroInWebViewTest
+//
+//  Created by Alan Hart on 25/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct GyroInWebViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
